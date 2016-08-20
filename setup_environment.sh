61e4b223a6391f1525aa7bfc2b5836701dc3ec33
bobbywlindsey <me@bobbywlindsey.com>
2016-08-19 23:52:31 -0500

    added vlc, gdebi-core, inconsolata font, R Open, MLK, and RStudio

--- setup_environment.sh
+++ setup_environment.sh
@@ -26,12 +26,26 @@ sudo apt-get install texstudio -y
 sudo apt-get install clamav clamav-daemon -y
 sudo freshclam
 sudo apt-get install xserver-xorg-input-libinput -y
-sudo apt-get install exfat-utils exfat-fuse
+sudo apt-get install exfat-utils exfat-fuse -y
+sudo apt-get install vlc -y
+sudo apt-get install gdebi-core
 # install Julia
 sudo add-apt-repository ppa:staticfloat/juliareleases
 sudo add-apt-repository ppa:staticfloat/julia-deps
 sudo apt-get update
-sudo apt-get install julia
+sudo apt-get install julia -y
+# install inconsolata
+sudo apt-get install fonts-inconsolata -y
+sudo fc-cache -fv
+# install R Open
+sudo apt-get install MRO-3.3.0-Ubuntu-15.4.x86_64.deb -y
+# download MLK (install later in script)
+wget https://mran.microsoft.com/install/mro/3.3.0/RevoMath-3.3.0.tar.gz
+
+# install RStudio
+wget https://download1.rstudio.org/rstudio-0.99.903-amd64.deb
+sudo gdebi -n rstudio-0.99.903-amd64.deb
+rm rstudio-0.99.903-amd64.deb
 # enable reverse mouse scrolling
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 
@@ -44,9 +58,16 @@ cp -rf ~/Downloads/.atom ~/
 # install meteor
 curl https://install.meteor.com/ | sh
 
-# install Anaconda (do this last as install takes user input)
+# install Anaconda (follow prompts)
 wget http://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
 bash Anaconda3-4.1.1-Linux-x86_64.sh
+# install MLK (follow prompts)
+tar -xzf RevoMath-3.3.0.tar.gz
+rm RevoMath-3.3.0.tar.gz
+cd RevoMath/
+sudo ./RevoMath.sh
+cd ..
+rm -rf RevoMath/
 
 # (install these Python packages after Anaconda installation)
 # pip install mkdoc mkdoc-material python-markdown-math
