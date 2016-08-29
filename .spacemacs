dc70df728d60b5358779b1df8b6fb8c29ad58841
bobbywlindsey <me@bobbywlindsey.com>
2016-08-29 15:07:12 -0500

    added react libraries; relative line numbering

--- .spacemacs
+++ .spacemacs
@@ -298,7 +298,9 @@ you should place your code here."
   (require 'ox-reveal)
   (require 'ox-twbs)
   ;; enable relative line numbers
-  dotspacemacs-line-numbers 'relative
+  (global-linum-mode)
+  (with-eval-after-load 'linum
+    (linum-relative-toggle))
   ;; disable global line highlighting
   (global-hl-line-mode -1)
   (with-eval-after-load 'javascript
--- linux/setup_env.sh
+++ linux/setup_env.sh
@@ -92,6 +92,35 @@ npm install -g jshint
 npm install -g eslint
 npm install -g babel-eslint
 npm install -g eslint-plugin-react
+# install js node packages for react dev
+npm install -g browserify
+npm install -g gulp
+npm install -g jshint
+npm install -g uglify-js
+npm install -g static-server
+npm install gulp-jshint jshint-stylish --save-dev
+npm install gulp-sass --save-dev
+npm install gulp-sourcemaps --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-react --save-dev
+npm install gulp-html-replace --save-dev
+npm install react react-dom babelify babel-preset-react --save-dev
+npm install bootstrap-sass --save-dev
+npm install jquery --save-dev
+npm install browserify --save-dev
+npm install babel-preset-es2015 --sav-dev
+npm install react-dom --save-dev
+npm install browser-sync gulp --save-dev
+npm install browser-sync --save-dev
+npm install del --save-dev
+npm install watchify --save-dev
+npm install gulp-notify --save-dev
+npm install superagent --save-dev
+npm install glob --save-dev
+npm install co --save-dev
+npm install react-router --save-dev
+npm install react-validate-decorator --save-dev
+npm install classnames --save-dev
 
 # install spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
