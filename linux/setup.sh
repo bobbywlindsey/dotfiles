29fd40347eab4ada932e306251bf1bf7e85942c2
bobbywlindsey <me@bobbywlindsey.com>
2018-07-25 16:59:24 -0500

    added DataGrip

--- linux/setup.sh
+++ linux/setup.sh
@@ -116,7 +116,10 @@ sudo apt update && sudo apt install signal-desktop -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client -y
+sudo apt-get install spotify-client -y --allow-unauthenticated
+
+# install DataGrip
+sudo snap install datagrip --classic
 
 # install fish
 sudo apt-add-repository ppa:fish-shell/release-2 -y
