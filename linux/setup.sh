899a08ce68dace315076ee43c6b4057641c2c5bd
bobbywlindsey <contact@bobbywlindsey.com>
2020-12-15 22:45:50 -0600

    Set custom keyboard shortcut to take an area screenshot and paste to clipboard

--- linux/setup.sh
+++ linux/setup.sh
@@ -15,9 +15,9 @@ sudo apt install -y \
     htop \
     unrar \
     tree
-flatpak install flathub com.visualstudio.code -y
 
 # Install media tools
+sudo apt install maim
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub md.obsidian.Obsidian -y
@@ -74,6 +74,12 @@ cp -R ./fish/functions/. ~/.config/fish/functions/
 /usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot "['<Shift><Alt>dollar']"
 /usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot-clip "['<Primary><Shift><Alt>dollar']"
 
+# Set keybinding for screenshots
+/usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+/usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Area screenshot to clipboard'"
+/usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Ctrl><Shift><Alt>dollar'"
+/usr/bin/gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'maim -s | xclip -selection clipboard -t image/png'"
+
 # Set default location for screenshots to Pictures
 /usr/bin/gsettings set org.gnome.gnome-screenshot auto-save-directory "/home/$USER/Desktop"
 
@@ -89,4 +95,3 @@ cp -R ./fish/functions/. ~/.config/fish/functions/
 
 # Enable reverse mouse scrolling
 /usr/bin/gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
-
