dd2d0d2c9f920e62d077e2c1e1ddfbece9325ca8
Bobby Lindsey <contact@bobbywlindsey.com>
2020-12-22 21:53:18 -0600

    Add imagemagick install

--- linux/media.sh
+++ linux/media.sh
@@ -13,3 +13,4 @@ flatpak install flathub net.ankiweb.Anki -y
 sudo apt install exfat-utils exfat-fuse -y
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo apt install imagemagick -y
