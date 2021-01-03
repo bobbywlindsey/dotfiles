cbcd38803a8da98a972776b2702ee40778772d3d
Bobby Lindsey <contact@bobbywlindsey.com>
2021-01-03 14:16:19 -0600

    Update thresholds

--- linux/opt/on_ac.sh
+++ linux/opt/on_ac.sh
@@ -8,6 +8,6 @@ set -e
 # https://www.reddit.com/r/System76/comments/kcqt9o/how_to_enable_and_control_charging_thresholds_on/
 
 # Enable safe charging limits.
-echo 15 >/sys/class/power_supply/BAT0/charge_control_start_threshold
-echo 96 >/sys/class/power_supply/BAT0/charge_control_end_threshold
+echo 68 >/sys/class/power_supply/BAT0/charge_control_start_threshold
+echo 81 >/sys/class/power_supply/BAT0/charge_control_end_threshold
 echo "Safe Charge Enabled - 80%"
