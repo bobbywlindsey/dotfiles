15033ad351e51c058bf6680238b3e6fb0accb9f7
Bobby Lindsey <contact@bobbywlindsey.com>
2025-10-15 12:26:17 -0600

    Set colorscheme to match terminal theme

--- nixos/nvim/.config/nvim/lua/bobby/colors.lua
+++ nixos/nvim/.config/nvim/lua/bobby/colors.lua
@@ -10,16 +10,20 @@ function ColorMyPencils()
                 then
                     --vim.cmd.colorscheme("rose-pine-dawn")
                     --vim.cmd.colorscheme("catppuccin-latte")
-                    vim.cmd.colorscheme("rose-pine-moon")
+                    --vim.cmd.colorscheme("rose-pine-moon")
+                    vim.cmd.colorscheme("catppuccin-mocha")
                     --vim.api.nvim_set_hl(0, "LineNr", { fg = "#9AA9B8" })
                     --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
                     --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
             else
-                vim.cmd.colorscheme("rose-pine-moon")
+                --vim.cmd.colorscheme("rose-pine-moon")
+                vim.cmd.colorscheme("catppuccin-mocha")
             end
         end
     })
 
 end
 
+-- Set default theme for netrw
+vim.cmd("colorscheme catppuccin-mocha")
 ColorMyPencils()
--- nixos/nvim/.config/nvim/lua/bobby/plugins/rose_pine.lua
+++ nixos/nvim/.config/nvim/lua/bobby/plugins/rose_pine.lua
@@ -1,7 +1,4 @@
 return {
     "rose-pine/neovim",
     name = "rose-pine",
-    config = function()
-      vim.cmd("colorscheme rose-pine")
-    end
 }
