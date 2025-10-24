ba739fcc3d324dcfac13ff9dd3eefe013d9dd3a6
Bobby Lindsey <contact@bobbywlindsey.com>
2025-10-24 15:48:20 -0600

    Fix bug in render_markdown module

--- nixos/nvim/.config/nvim/lua/bobby/plugins/render_markdown.lua
+++ nixos/nvim/.config/nvim/lua/bobby/plugins/render_markdown.lua
@@ -1,4 +1,4 @@
-Freturn {
+return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
     ---@module 'render-markdown'
