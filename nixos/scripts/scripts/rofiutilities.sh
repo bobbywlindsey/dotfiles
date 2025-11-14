0010244c8245dc25286167f825c471053b1da5d5
Bobby Lindsey <contact@bobbywlindsey.com>
2025-11-14 16:41:14 -0700

    Add utilities rofi menu

---
+++ nixos/scripts/scripts/rofiutilities.sh
@@ -0,0 +1,20 @@
+calc () {
+    input=$(rofi -dmenu -i -p "=" -lines 1) || exit 0
+    result=$(echo "$input" | bc)
+    result_choice=$(echo "$result" | rofi -dmenu -i -p "Result:")
+    
+    # If the user selected the result, copy it to the clipboard
+    if [[ -n "$result_choice" ]]; then
+        echo "$result_choice" | tr -d '[:space:]' | wl-copy
+        notify-send "Copied $result_choice to clipboard"
+    fi
+}
+
+menu() {
+    CHOICE=$(printf "  Calc=\\n" | rofi -dmenu -i -lines 21 -location 1 )
+    case "$CHOICE" in 
+        **) calc ;;
+    esac
+}
+
+menu
