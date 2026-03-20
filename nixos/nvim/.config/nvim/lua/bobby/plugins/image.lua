41d8fdec7b33327fc2368840a8b3328da2c05f7a
Bobby Lindsey <contact@bobbywlindsey.com>
2026-03-20 09:42:57 -0600

    Add support for images in markdown

--- nixos/nvim/.config/nvim/lua/bobby/plugins/image.lua
+++ nixos/nvim/.config/nvim/lua/bobby/plugins/image.lua
@@ -3,5 +3,11 @@ return {
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {
         processor = "magick_cli",
+        integrations = {
+            markdown = {
+                only_render_image_at_cursor = true,
+                only_render_image_at_cursor_mode = "popup",
+            },
+        },
     }
 }
