f89382d351acccf388849d72b9d298e7fd7e6f64
Bobby Lindsey <contact@bobbywlindsey.com>
2026-05-31 10:59:05 -0600

    Don't sleep while charging

--- nixos/hypr/.config/hypr/switch.sh
+++ nixos/hypr/.config/hypr/switch.sh
@@ -5,6 +5,6 @@ if grep open /proc/acpi/button/lid/LID0/state; then
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1,disable"
-        swww img ~/Pictures/wallpaper.jpg &
+        awww img ~/Pictures/wallpaper.jpg &
     fi
 fi
