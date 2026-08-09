90ef1dcf8c8eeafd121177161a41466e51335d41
Bobby Lindsey <contact@bobbywlindsey.com>
2026-08-09 11:57:17 -0600

    Update swayimg lua config

--- nixos/swayimg/.config/swayimg/init.lua
+++ nixos/swayimg/.config/swayimg/init.lua
@@ -10,15 +10,8 @@ end)
 swayimg.viewer.on_key("Ctrl-t", function()
   local img = swayimg.viewer.current_image()
   local path = img["path"]
-  os.execute(string.format([[
-    kitty -e bash -c '
-      orig=$(exiftool -DateTimeOriginal -s3 -d "%%Y:%%m:%%d %%H:%%M:%%S" %q)
-      old_date=$(echo "$orig" | cut -d" " -f1)
-      old_time=$(echo "$orig" | cut -d" " -f2)
-      read -e -i "$old_date" -p "New date (YYYY:MM:DD): " new_date
-      new_datetime="$new_date $old_time"
-      exiftool "-DateTimeOriginal=$new_datetime" "-CreateDate=$new_datetime" %q
-      read -p "Press enter to close..."
-    '
-  ]], path, path))
+  os.execute(string.format(
+    "hyprctl dispatch exec -- kitty -e ~/.config/swayimg/edit_date.sh %q",
+    path
+  ))
 end)
